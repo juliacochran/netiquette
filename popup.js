897c4a6f854c09826dca291223c1042e83fc23f7
Julia Cochran <juliacochran7@gmail.com>
2013-07-06 19:11:49 +0200

    added some styling. content scripts still not working

--- popup.js
+++ popup.js
@@ -1,2 +1,9 @@
-//for displaying popup content
-document.getElementById('tips').innerHTML = "[tips for etiquette displayed here eventually]"
+//definitely not correct. attempting to listen to message from content script
+chrome.extension.onRequest.addListener(function(keywords, sender, sendResponse) {
+	if(keywords.length > 0)
+		//chrome.pageAction.show(sender.tab.id);
+		document.getElementById('tips').innerHTML = 'keywords is not empty. woooooo!'
+	sendResponse();
+});
+
+
---
+++ testscript.js
@@ -0,0 +1,2 @@
+var replaced = $("body").html().replace(/text/g,'replace');
+$("body").html(replaced);
