edb1f5b481a296f3277ed0b80b22616eab7e4e91
Julia Cochran <juliacochran7@gmail.com>
2013-07-06 19:38:50 +0200

    Delete popup.js

--- popup.js
+++
@@ -1,9 +0,0 @@
-//definitely not correct. attempting to listen to message from content script
-chrome.extension.onRequest.addListener(function(keywords, sender, sendResponse) {
-	if(keywords.length > 0)
-		//chrome.pageAction.show(sender.tab.id);
-		document.getElementById('tips').innerHTML = 'keywords is not empty. woooooo!'
-	sendResponse();
-});
-
-
