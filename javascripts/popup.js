9721cefe165b916366759d0a2607baa98faaa78c
Tyler Lucas <rtylerlucas@gmail.com>
2013-07-08 00:40:53 +0200

    Filter URL attempt
    Still not working, need to insert url variable into dom.

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -3,10 +3,29 @@
 //once we get the keywords back, algorithm for determining the category will go in this file 
 //then we will be able to set the content accordingly
 
-chrome.extension.onRequest.addListener(function(keywords, sender, sendResponse) {
-	if(keywords.length > 0)
-		document.getElementById('tips').innerHTML = 'keywords is not empty. woooooo!'
-	sendResponse();
+//chrome.extension.onRequest.addListener(function(keywords, sender, sendResponse) {
+//	if(keywords.length > 0)
+//		document.getElementById('tips').innerHTML = 'keywords is not empty. woooooo!'
+//	sendResponse();
+//});
+//Still working on this can't get it functioning properly...
+
+// Do NOT forget that the method is ASYNCHRONOUS
+
+chrome.tabs.query(
+    {
+    active: true,                              // Select active tabs
+    windowId: chrome.windows.WINDOW_ID_CURRENT // In the current window
+    },
+    function(array_of_Tabs) {
+    // Since there can only be one active tab in one active window,
+    //  the array has only one element
+    var tab = array_of_Tabs[0];
+    // Example:
+    var url = tab.url;
+    // ... do something with url variable
+
 });
+
 
 
