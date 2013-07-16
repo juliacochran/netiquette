cb84040fb2481f6c530a78d26a261af2d7c6d990
Julia Cochran <juliacochran7@gmail.com>
2013-07-16 22:29:18 +0200

    added social networking html

--- javascripts/contentscript.js
+++ javascripts/contentscript.js
@@ -1,19 +1,19 @@
 //content script that will eventually get the meta data from the page and convey that information to popup.js for processing
-//var port = chrome.runtime.connect();
 
-var meta_keywords = getKeywords();
 
-//should notify extension of keywords found
-chrome.extension.sendRequest(meta_keywords);
+function onContextClick(info, tab) {    
+  if( info["selectionText"] ){  
+    getMeta(tab, function(x){
+      console.log(x);
+      //do stuff with x
+    });
+  }
+}
 
-function getKeywords() {
-	data = document.getElementsByTagName("meta");
-	keywords = []; 
-	var len = data.length;
-	for (var i = 0; i < len; i++) {
- 	   if (data[i].name == 'keywords') {
-  	      keywords = data[i].content.split(",");
-  	 	 }
-	}
-	return keywords;
+function getMeta (tab, callback) {
+chrome.tabs.sendMessage(tab.id, {fetchTag: "meta,name,author,content"}, function(response) {
+    //alert(response.data);
+    //one thing I tired was to put my "do stuff" embedded here, but that didn't work either
+    callback(response.data);
+    });
 }
